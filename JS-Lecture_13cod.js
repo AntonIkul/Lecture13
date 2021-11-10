466a066bbd5223cafe029ea9f9008b42577511fd
AntonIkul <Kulazhenko2000@mail.ru>
2021-11-10 23:45:56 +0300

    Connect with lectuere 13

---
+++ JS-Lecture_13cod.js
@@ -0,0 +1,33 @@
+"use strict";
+
+if (4==4){
+    console.log("Very good");
+}else{
+    console.log("Error");
+}
+////////////////////////////////////
+const num = 80;
+if (num <79){
+    console.log("so little");
+}else if (num>81){
+    console.log("Very many");
+}else{
+    console.log("Very good");
+}
+/////////////////////////////////////
+const  cifra = "50";
+
+switch (cifra){
+    case "49":
+        console.log("Error");
+        break;
+    case "51":
+        console.log("MAny");
+        break;
+     case "50":
+        console.log("Goood");
+        break;
+    default:
+            console.log("Nothing came up");
+            break;    
+}
